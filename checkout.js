80d6af6b79c7e0efb0f8f1ee7f28b32f2cb634cd
Ruwayne <137304473+Humblefrib@users.noreply.github.com>
2024-09-12 23:49:12 +0200

    Delete checkout.js

--- checkout.js
+++
@@ -1,8 +0,0 @@
-const checkoutElements = document.querySelectorAll('.checkOut');
-    
-// Add a click event listener to each of them
-checkoutElements.forEach(function(element) {
-    element.addEventListener('click', function() {
-        console.log('Checkout button clicked!');
-    });
-});
