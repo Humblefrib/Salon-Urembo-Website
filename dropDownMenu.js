fd34d10939dd18dcdcc628bb2fbc4dc6b3daa416
Ruwayne <137304473+Humblefrib@users.noreply.github.com>
2024-10-03 17:55:06 +0200

    Update dropDownMenu.js

--- dropDownMenu.js
+++ dropDownMenu.js
@@ -1,6 +1,13 @@
 let navSvg = document.querySelector('.navSvg');
 let dropDownMenu = document.querySelector(".dropDownMenu");
+let navLinks = document.querySelectorAll(".dropDownMenu div");
 
 navSvg.addEventListener('click', () => {
     dropDownMenu.classList.toggle('active');
 });
+
+navLinks.forEach(navLink => {
+    navLink.addEventListener("click", () => {
+        dropDownMenu.classList.toggle('active');
+    });
+})
